feda5bb509f34a44eb2974492457a82bd20fb2e1
SWH <slgo123@naver.com>
2019-11-01 02:59:26 +0900

    1101up

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 
 function Jsxthe(props){
-  console.log(props);
-  return <h3> 안녕 그지 깽깽이들아.</h3>
+  console.log(props.fav);
+  return <h3> 안녕 그지 깽깽이들아. 나는 {props.fav} 를 좋아해.</h3>
 }
 
 function App() {
   return (
     <div> 
       <h1>여기는 app 안이야.</h1>
-      <Jsxthe fav="kimchi"  />
+      <Jsxthe fav="kimchi" />
+      <Jsxthe fav="쭈꾸미" />
+      <Jsxthe fav="삼겹살" />
+      <Jsxthe fav="kimchi" />
+      
     </div>
 
     );

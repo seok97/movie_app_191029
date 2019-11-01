9a7786aae23ae45a330ef3a0c94b9066a96dad8f
SWH <slgo123@naver.com>
2019-11-01 21:43:20 +0900

    js map !

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import React from 'react';
+import { ENOTCONN } from 'constants';
 
 function Jsxthe(props){
-  console.log(props.fav);
-  return <h3> 안녕 그지 깽깽이들아. 나는 {props.fav} 를 좋아해.</h3>
+  console.log(props.name);
+  return (
+  <h3> 내가 좋아하는 아이돌은 {props.name} 이야. <br></br><img src={props.shit} /> </h3>
+  );
 }
+
+const foodlke = [
+  { 
+    name: "itzy",
+    shit: "https://farm8.staticflickr.com/7896/46731686204_51bdb425e3_o.jpg"
+  },
+  { 
+    name: "twice",
+    shit: "https://farm5.staticflickr.com/4819/46713125631_3f1356b6cf_o.jpg"
+  },
+  { 
+    name: "fromis",
+    shit: "https://live.staticflickr.com/65535/48013082297_623af4ef20_o.jpg"
+  }
+
+]
 
 function App() {
   return (
     <div> 
       <h1>여기는 app 안이야.</h1>
-      <Jsxthe fav="kimchi" />
-      <Jsxthe fav="쭈꾸미" />
-      <Jsxthe fav="삼겹살" />
-      <Jsxthe fav="kimchi" />
-      
+      <p>{foodlke.map( etc => ( <Jsxthe name={etc.name} shit={etc.shit} />)) }</p>
     </div>
 
     );
